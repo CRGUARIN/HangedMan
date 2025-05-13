a20915f490d6874b8e0973cca66e1a409c87704d
CRGUARIN <crguarinm@gmail.com>
2025-05-12 19:02:54 -0500

    + countWords() loadWords()

--- ahorcado.py
+++ ahorcado.py
@@ -1,6 +1,6 @@
 """
-Angela Lisette Acosta Chaucanés
 Cristian Alejandro Guarin Marin
+Grupo: 3
 """
 
 def printIntro(fileName):
@@ -44,7 +44,6 @@ def printIntro(fileName):
     except Exception as e:
         print(f"Se produjo un error al leer el archivo: {e}")
 
-
 def inputSecret():
     '''
     Firma:
@@ -69,4 +68,61 @@ def inputSecret():
     # Cuerpo de la función
     secret = input("Ingrese la palabra o frase oculta: ")
     return secret
+
+def loadWords(a):
+    '''
+    Firma:
+        (string) -> (string)
+        
+    Sinopsis:
+        Función que solicita el nombre de un archivo cualquiera y devuelve una cadena 
+        de caracteres con su contenido
+        
+    Entradas y salidas:
+        - filename: string que contiene el nombre del archivo con las palabras secretas
+        - returns: string con todas las palabras secretas
+        
+    Ejemplos de uso:
+        >>> loadWords("superHeroes.txt")
+        'capitan centella, capitan planeta, batman, superman, robin, mujer maravilla, aquaman, flash,
+        cyborg, capitan marciano, linterna verde,flash gordon, liga de la justicia, defensores de la 
+        tierra, el fantasma, spider man, hulk, thor, iron man, los vengadores, robocop, terminator, 
+        capitan america, hombre hormiga, la avispa, goku, vegeta, gohan, piccolo, trunks, spawn, 
+        tintin, ghost rider, blade, tortugas ninja, soldado del invierno, el castigador, 
+        el predicador, leonidas, kick ass, el comediante, el chapulin colorado, wolverine,
+        flecha verde, el profesor super o, los autobots, robin hood\n'
+    '''
+
+    try: 
+        text = open(f"{a}","r", encoding="utf-8")
+        content = text.read()
+        return content
+    except FileNotFoundError:
+        print("El archivo al que quiere acceder no existe o ingresó mal la ruta")
+
+def countWords(a, b):
+    '''
+    Firma:
+        (string,string) -> (int)
+        
+    Sinopsis:
+        Función que cuenta la cantidad de palabras disponibles en una cadena
+        
+    Entradas y salidas:
+        - palabras: Conjunto de palabras o frases separadas por un delimitador
+        - separador: Delimitador que separa una palabra o frase de otra dentro de la cadena de entrada (palabras)
+        - returns: cantidad de palabras secretas
+        
+    Ejemplos de uso:
+        >>> materias = 'español,calculo,geometria vectorial,geometria euclidiana'
+        >>> countWords(materias,',')
+        4
+        
+        >>> countWords('gallo-gallina','-')
+        2
+    '''
+    divided = a.split(b)
+    quantity = len(divided)
+    return quantity
+
 
--- pruebas.py
+++ pruebas.py
@@ -1,20 +1,35 @@
 """
-Angela Lisette Acosta Chaucanés
 Cristian Alejandro Guarin Marin
+Grupo: 3
 """
 
-import ahorcado as ah
+import ahorcado as fc
 
 #Pruebas de la función printIntro
 
-ah.printIntro("/home/crguarinm/Documentos/UdeA/Informática 1/Unidad 4/HangedMan/intro.txt")
+#fc.printIntro("/home/crguarinm/Documentos/UdeA/Informática 1/Unidad 4/HangedMan/intro.txt")
 
-ah.printIntro("/home/crguarinm/Documentos/UdeA/Informática 1/Unidad 4/HangedMan/main.py")
+#fc.printIntro("/home/crguarinm/Documentos/UdeA/Informática 1/Unidad 4/HangedMan/main.py")
 
 #Pruebas de la función inputSecret
 
-h = ah.inputSecret()
-print(h)
+#h = fc.inputSecret()
+#print(h)
 
-l = ah.inputSecret()
-print(l)
+#l = fc.inputSecret()
+#print(l)
+
+#Pruebas de la función loadWords
+
+#superheroes = fc.loadWords("/home/crguarinm/Documentos/UdeA/Informática 1/Unidad 4/HangedMan/superHeroes.txt")
+#print(superheroes)
+
+#generalidades = fc.loadWords("/home/crguarinm/Documentos/UdeA/Informática 1/Unidad 4/Generalidades lab 4.txt")
+#print(generalidades)
+
+#Pruebas countWords
+
+a = input("Ingrese una frase: ")
+b = input("Ingrese el separador entre las palabras: ") 
+c = fc.countWords(a, b)
+print(c)
